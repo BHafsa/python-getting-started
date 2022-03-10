2f23d37532f098bae1e7d146ed734ec121c4ce78
Bhafsa <eh_bousbiat@esi.dz>
2022-03-10 09:30:56 +0100

    'test'

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.system('mlflow ui')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 
+application = get_wsgi_application()
--- hello/views.py
+++ hello/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-
+import os
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
+    os.system('mlflow ui')
     return render(request, "index.html")
 
 
