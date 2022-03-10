309e4f1ea561dc396afe5be942d9d3a6630c03c3
Bhafsa <eh_bousbiat@esi.dz>
2022-03-10 09:22:19 +0100

    'test'

--- gettingstarted/wsgi.py
+++ gettingstarted/wsgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+os.system('mlflow ui')
 
-from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
